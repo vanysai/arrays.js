ce48ac824337d73df1e1bc80e90acf9b28ba4d7f
Jaíza Freire <99412036+vanysai@users.noreply.github.com>
2022-05-18 21:02:35 -0300

    Update exercicios.js

--- exercicios.js
+++ exercicios.js
@@ -11,3 +11,51 @@ console.log(salarios)
 
 let booleans = [8 > 2, 78 > 3, 45 < 5]
 console.log(booleans)
+
+//4 
+
+let salarios2 = ['R$900', 'R$800', 'R$20000' , 'R$1000']
+console.log(salarios2)
+
+//5
+
+Q5pre = [10.0,29.9,32.6];
+Q5 = [];
+for(i of Q5pre){
+    Q5.push(i)
+    Desconto = i * (95/100)
+    Q5.push(Desconto);
+    console.log(Q5)
+}
+
+//6
+
+let matriz = [[2, 4, 8],[6, 9, 2], [6, 8, 6]]
+for(let i in matriz){
+    console.log("|", matriz[i][0], matriz[i][1], matriz[i][2], "|")
+}
+console.log('MATRIZ DOBRADA')
+for (let i in matriz){
+  
+    matriz[i][i] *= 2
+    console.log("|", matriz[i][0], matriz[i][1], matriz[i][2], "|") 
+}
+
+//7
+
+function random(max,min){
+    n = Math.floor(Math.random()* (max - min) + min);
+    return n;
+}
+q7 = []
+for(i = 0; i < 2; ii++){
+    arr = []
+q7.push(arr);
+for(ii = 0; ii < 2; ii++){
+    q7[i].push(random(9,0));
+}
+}
+det = (q7[0][0]* q7[1][1] - q7[0][1] * q7[1][0]);
+console.log(det);
+
+// não sei se a 7 ta certa (???)
